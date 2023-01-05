4489ca0185a5cb4a8ae6dc35b6b45f048acc9f22
yqiu <qiu5630@163.com>
2023-01-05 23:49:04 +0800

    Initial Commit

---
+++ CheckList/CheckListApp.swift
@@ -0,0 +1,17 @@
+//
+//  CheckListApp.swift
+//  CheckList
+//
+//  Created by 邱永劲 on 2023/1/5.
+//
+
+import SwiftUI
+
+@main
+struct CheckListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
